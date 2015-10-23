adcb85d8260094a8d81dbc8853d80a0f93438c4b
gorishagh <goris.haghverdian@yahoo.com>
2015-10-23 13:41:23 -0700

    Fix in route

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'item#index', as: 'item'
+  root 'item#index', as: 'Item'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
