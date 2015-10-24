b28c6e324b06fad163c119a3c8b4f6d32e4d899e
gorishagh <goris.haghverdian@yahoo.com>
2015-10-24 12:01:50 -0700

    added table scaffold.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151022002618) do
+ActiveRecord::Schema.define(version: 20151024185530) do
 
   create_table "items", force: :cascade do |t|
     t.string   "title",       limit: 255
@@ -20,6 +20,12 @@ ActiveRecord::Schema.define(version: 20151022002618) do
     t.decimal  "price",                     precision: 8, scale: 2
     t.datetime "created_at",                                        null: false
     t.datetime "updated_at",                                        null: false
+  end
+
+  create_table "tables", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
   end
 
 end
