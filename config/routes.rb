3e9d102872297eea0c0f1e14d368d52cef286c46
gorishagh <goris.haghverdian@yahoo.com>
2015-10-23 18:31:52 -0700

    routes file back to its original.

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-  get 'item/index'
+  get 'store/index'
 
   resources :items
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'items#index', as: 'Item'
+  root 'store#index', as: 'store'
 
   # Example of regular route: 
   #   get 'products/:id' => 'catalog#view'
