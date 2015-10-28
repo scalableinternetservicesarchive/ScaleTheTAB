482b9f3fa7b63ee00cb94235841920215e2725ae
Live session user <harshjain30@gmail.com>
2015-10-27 23:37:45 -0700

    added relationships

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,8 @@
 class Item < ActiveRecord::Base
 	belongs_to :menu
+	has_many :line_items
+
+	before_destroy :ensure_not_referenced_by_any_line_item
 
 
 
@@ -10,4 +13,17 @@ class Item < ActiveRecord::Base
 		with: %r{\.(gif|jpg|png\Z)}i,
 		message: 'must be a URL for GIF, JPG, PNG'
 	}
+
+
+	private
+
+  # ensure that there are no line items referencing this product
+    def ensure_not_referenced_by_any_line_item
+      if line_items.empty?
+	return true
+      else
+	errors.add(:base, 'Line Items present')
+	return false
+      end
+    end
 end
