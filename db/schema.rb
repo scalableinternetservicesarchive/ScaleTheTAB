743bd773bdc1d4072a54aecc9834871b9ecaf3a3
Live session user <harshjain30@gmail.com>
2015-10-28 00:45:00 -0700

    added quantities for cart

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151028060640) do
+ActiveRecord::Schema.define(version: 20151028073226) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -31,8 +31,9 @@ ActiveRecord::Schema.define(version: 20151028060640) do
   create_table "line_items", force: :cascade do |t|
     t.integer  "item_id",    limit: 4
     t.integer  "cart_id",    limit: 4
-    t.datetime "created_at",           null: false
-    t.datetime "updated_at",           null: false
+    t.datetime "created_at",                       null: false
+    t.datetime "updated_at",                       null: false
+    t.integer  "quantity",   limit: 4, default: 1
   end
 
   add_index "line_items", ["cart_id"], name: "index_line_items_on_cart_id", using: :btree
