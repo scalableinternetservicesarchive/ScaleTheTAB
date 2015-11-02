a09acb99d26c0699aee570f6f9c3d50a4cbdde41
Live session user <harshjain30@gmail.com>
2015-11-01 21:25:01 -0800

    added carts to tabs

--- app/models/tab.rb
+++ app/models/tab.rb
@@ -1,2 +1,3 @@
 class Tab < ActiveRecord::Base
+  has_many :carts, dependent: :destroy
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,9 @@ Rails.application.routes.draw do
 
 
   resources :line_items
-  resources :carts
   
+  resources :carts
+
   resources :menus
   resources :restaurants
   resources :tables
@@ -24,6 +25,7 @@ Rails.application.routes.draw do
 
   # Example of regular route: 
   #   get 'products/:id' => 'catalog#view'
+  post "carts/add_to_order"
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
