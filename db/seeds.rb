8b61272eb07a39d48e2be00b2a335a2865a59502
gorishagh <goris.haghverdian@yahoo.com>
2015-11-13 00:10:27 -0800

    added some seed to the application for load testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+
+restaurant = Restaurant.create({name: 'Rest1', description: 'Rest1Desc'})
+
+restaurant.tables.create([{name: 'Table1'},{name: 'Table2'}])
+
+menu = restaurant.menus.create({title: 'menu1'})
+
+menu.items.create([{title: 'item1',price: 9.99},
+	{title: 'item2', price: 12.99},
+	{title: 'item3', price: 12.99},
+	{title: 'item4', price: 12.99},
+	{title: 'item5', price: 12.99}])
