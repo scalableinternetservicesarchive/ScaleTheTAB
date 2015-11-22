b74ce920fb1d02d7fd8b11d2c613079d99ad3227
Harsh <harshjain30@gmail.com>
2015-11-21 19:28:14 -0800

    adding check for user_signed_in

--- app/controllers/concerns/current_tab.rb
+++ app/controllers/concerns/current_tab.rb
@@ -5,8 +5,9 @@ module CurrentTab
   
     def set_tab
       @tab = Tab.find(session[:tab_id])
-    rescue ActiveRecord::RecordNotFound      
-      @tab = Tab.create({user_id: current_user.id, table_id: params[:table_id]})
+    rescue ActiveRecord::RecordNotFound
+      @user_id = current_user.id if user_signed_in? else 1
+      @tab = Tab.create({user_id: @user_id, table_id: params[:table_id]})
       session[:tab_id] = @tab.id
     end
 end
