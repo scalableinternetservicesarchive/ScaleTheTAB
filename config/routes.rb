3d940d48246910e200a7f3bed392aff844a04958
Nitish Mehta <Nitish@s-164-67-212-232.resnet.ucla.edu>
2015-11-24 00:26:58 -0800

    Added changes to the front page

--- config/routes.rb
+++ config/routes.rb
@@ -19,16 +19,16 @@ Rails.application.routes.draw do
   resources :menus
   resources :restaurants
   resources :tables
-  resources :menus
+
 
   get 'store/index'
-
+  root to: 'home#index'
   resources :items
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'home#index', as: 'Home'
+  # root 'home#index', as: 'Home'
 
   # Example of regular route: 
   #   get 'products/:id' => 'catalog#view'
