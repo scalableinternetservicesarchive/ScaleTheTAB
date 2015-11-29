28a011c12a0749cffafc1b6d63e93678d2a22841
Nitish Mehta <Nitish@Nitishs-MacBook-Pro.local>
2015-11-29 03:00:31 -0800

    changes messages going to owner

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,26 +3,26 @@ module ApplicationHelper
   		content_for(:page_header) { text.to_s }
 	end
 
-	def sendmsg(owner_id, msg)
+	def sendmsg(owner_id, table_id, msg)
   		recipients = Owner.where(id: owner_id)
-      puts "*********IN sendmsg***************************************************************"
-      # puts owner_id
-      # puts msg
-      puts "Recipient: "
-      puts recipients.inspect
-      # puts current_owner
-      puts "Current USer:"
+      # puts "*********IN sendmsg***************************************************************"
+      # # puts owner_id
+      # # puts msg
+      # puts "Recipient: "
+      # puts recipients.inspect
+      # # puts current_owner
+      # puts "Current USer:"
       
       if not user_signed_in?
-        puts "User has not signed in "
+        # puts "User has not signed in "
         temp = User.find(1)
-        puts temp.inspect
+        # puts temp.inspect
       else
         temp = current_user
-        puts current_user.inspect
+        # puts current_user.inspect
         
       end
-      conversation = temp.send_message(recipients, msg , "Order").conversation
+      conversation = temp.send_message(recipients, msg , table_id).conversation
       flash[:success] = "Message has been sent!"
     end
   	
