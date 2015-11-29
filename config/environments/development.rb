0ea1116e252565d7a4861041d0b366f048b4f748
Nitish Mehta <Nitish@Nitishs-MacBook-Pro.local>
2015-11-28 19:05:45 -0800

    Added aws.yml to check if images getting saved on s3

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,5 +41,12 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   
-  Paperclip.options[:command_path] = "/usr/local/bin/"
+  # Paperclip.options[:command_path] = "/usr/local/bin/"
+   config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => 'scalableinternetservices/ScaleTheTAB',
+      :s3_credentials => "#{Rails.root}/config/aws.yml",
+    }
+  }
 end
