ac0d18f28134e36b4629b68027d18bee870baf56
Nitish Mehta <Nitish@Nitishs-MacBook-Pro.local>
2015-11-28 16:58:20 -0800

    messaging done without bugs!

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,17 +5,25 @@ module ApplicationHelper
 
 	def sendmsg(owner_id, msg)
   		recipients = Owner.where(id: owner_id)
-  		puts "*********IN sendmsg**********"
-  		# puts owner_id
-  	  puts msg
-  		puts recipients.inspect
-  		# puts current_owner
-      # if not user_signed_in?
-      #   current_user = User.find(id: 1)
-      # end
-      puts "*** CURENT USER"
-      puts current_user.inspect
-   	  conversation = current_owner.send_message(recipients, msg , "Order").conversation
+      puts "*********IN sendmsg***************************************************************"
+      # puts owner_id
+      # puts msg
+      puts "Recipient: "
+      puts recipients.inspect
+      # puts current_owner
+      puts "Current USer:"
+      
+      if not user_signed_in?
+        puts "User has not signed in "
+        temp = User.find(1)
+        puts temp.inspect
+      else
+        temp = current_user
+        puts current_user.inspect
+        
+      end
+      conversation = temp.send_message(recipients, msg , "Order").conversation
       flash[:success] = "Message has been sent!"
-  	end
+    end
+  	
 end
