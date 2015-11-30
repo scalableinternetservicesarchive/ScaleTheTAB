7dd86a8f12792e4009b6d72e15061c0c881ac1b2
Nitish Mehta <Nitish@s-169-232-178-229.resnet.ucla.edu>
2015-11-30 14:48:48 -0800

    Added automatic user, owner and rest creation

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,8 +9,14 @@
 require 'Faker'
 include Faker
 
+Restaurant.delete_all
 LineItem.delete_all
-Item.delete_all
+Cart.delete_all
+Checkout.delete_all
+Tab.delete_all
+User.delete_all
+Owner.delete_all
+
 
 # 1.times{
 # 	item = Item.create(
@@ -23,3 +29,47 @@ Item.delete_all
 # 	)
 # 	puts item.inspect
 # }
+
+total_no_of_users = 5
+total_no_of_owners = 5
+total_no_of_rest = 5
+
+
+sample_restaurant_image = File.open("#{Rails.root}/load-tests/res4.jpg")
+
+#***************************
+# Creating Users
+#***************************
+total_no_of_users.times{
+  user = User.create(
+    email: Internet.email,
+    password: "password"
+  )
+}
+
+#***************************
+# Creating Owners 
+#***************************
+total_no_of_owners.times{
+  owner = Owner.create(
+    email: Internet.email,
+    password: "password"
+  )
+}
+
+#***************************
+# Creating Restaurants
+#***************************
+total_no_of_rest.times{
+  restaurant = Restaurant.create(
+    name: Company.name,
+    image_url: "NULL",
+    description: Lorem.sentence(3),
+    address: Address.city,
+    city: Address.city_prefix,
+    zip_code: Address.zip_code,
+    tell: PhoneNumber.phone_number,
+    owner_id: rand(total_no_of_owners),
+    image_file_name: "res4.jpg"
+  )
+}
