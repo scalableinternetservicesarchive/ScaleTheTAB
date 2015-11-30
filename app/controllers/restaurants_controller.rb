69379fe41cb4e2d1b180accdb69340125e406544
Nitish Mehta <Nitish@Nitishs-MacBook-Pro.local>
2015-11-29 19:49:15 -0800

    rest index caching done!

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -37,6 +37,10 @@ class RestaurantsController < ApplicationController
     #Client Side Caching using etag
 
 
+
+    fresh_when(:etag => [@restaurants, current_user])
+
+
     fresh_when(:etag => [@restaurants, current_user])
 
 
@@ -57,9 +61,13 @@ class RestaurantsController < ApplicationController
 
     #Client side Caching using Etag
 
+
 		#fresh_when(:etag => [@restaurant, @menus])
 
   	#fresh_when(:etag => [@restaurant, @menus])
+
+
+		#fresh_when(:etag => [@restaurant, @menus])
 
 		#create tab and cart if user is signed in or is in guest mode AND table_id is set
 		if not owner_signed_in? and params[:table_id]
