212a6dfc72c5233a82b5374fb0fcd15d3546f31b
gorishagh <goris.haghverdian@yahoo.com>
2015-11-30 00:09:55 -0800

    seed added

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,4 +1,4 @@
 class Menu < ActiveRecord::Base
 	belongs_to :restaurant
-	has_many :items
+	has_many :items, :dependent => :destroy
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,29 +8,60 @@
 
 
 
-restaurant = Restaurant.create({name: 'Rest1', description: 'Rest1Desc'})
+# restaurant = Restaurant.create({name: 'Rest1', description: 'Rest1Desc'})
 
-restaurant.tables.create([{name: 'Table1'},{name: 'Table2'}])
+# restaurant.tables.create([{name: 'Table1'},{name: 'Table2'}])
 
-menu = restaurant.menus.create({title: 'menu1'})
+# menu = restaurant.menus.create({title: 'menu1'})
 
-menu.items.create([{title: 'item1',price: 9.99},
-	{title: 'item2', price: 12.99},
-	{title: 'item3', price: 12.99},
-	{title: 'item4', price: 12.99},
-	{title: 'item5', price: 12.99}])
-
-
-
-Owner.create({email: 'o1@seed.com' , password: '12345678' , password_confirmation: '12345678'})
-Owner.create({email: 'o2@seed.com' , password: '12345678' , password_confirmation: '12345678'})
-Owner.create({email: 'o3@seed.com' , password: '12345678' , password_confirmation: '12345678'})
-Owner.create({email: 'o4@seed.com' , password: '12345678' , password_confirmation: '12345678'})
-Owner.create({email: 'o5@seed.com' , password: '12345678' , password_confirmation: '12345678'})
+# menu.items.create([{title: 'item1',price: 9.99},
+# 	{title: 'item2', price: 12.99},
+# 	{title: 'item3', price: 12.99},
+# 	{title: 'item4', price: 12.99},
+# 	{title: 'item5', price: 12.99}])
 
 
-User.create({email: 'u1@seed.com' , password: '12345678' , password_confirmation: '12345678'})
-User.create({email: 'u2@seed.com' , password: '12345678' , password_confirmation: '12345678'})
-User.create({email: 'u3@seed.com' , password: '12345678' , password_confirmation: '12345678'})
-User.create({email: 'u4@seed.com' , password: '12345678' , password_confirmation: '12345678'})
-User.create({email: 'u5@seed.com' , password: '12345678' , password_confirmation: '12345678'})
+
+# Owner.create({email: 'o1@seed.com' , password: '12345678' , password_confirmation: '12345678'})
+# Owner.create({email: 'o2@seed.com' , password: '12345678' , password_confirmation: '12345678'})
+# Owner.create({email: 'o3@seed.com' , password: '12345678' , password_confirmation: '12345678'})
+# Owner.create({email: 'o4@seed.com' , password: '12345678' , password_confirmation: '12345678'})
+# Owner.create({email: 'o5@seed.com' , password: '12345678' , password_confirmation: '12345678'})
+
+
+# User.create({email: 'u1@seed.com' , password: '12345678' , password_confirmation: '12345678'})
+# User.create({email: 'u2@seed.com' , password: '12345678' , password_confirmation: '12345678'})
+# User.create({email: 'u3@seed.com' , password: '12345678' , password_confirmation: '12345678'})
+# User.create({email: 'u4@seed.com' , password: '12345678' , password_confirmation: '12345678'})
+# User.create({email: 'u5@seed.com' , password: '12345678' , password_confirmation: '12345678'})
+
+
+item_num = 0
+Menu.delete_all
+Item.delete_all
+Owner.delete_all
+Restaurant.delete_all
+User.delete_all
+
+#Item.delete_all
+for i in 0..1000 do
+	owner = Owner.create({email: "o#{i}@seed.com" , password: '12345678' , password_confirmation: '12345678'})
+	restaurant = Restaurant.create({name: "Rest #{i}", description: "Rest #{i} Desc" , address: '123 Somewhere' , 
+		city: 'Los Angeles' , zip_code: '90001' , tell: "8001112233" , owner_id: owner.id})
+	restaurant.tables.create([{name: 'Table1'},{name: 'Table2'}])
+
+	menu = restaurant.menus.create({title: 'menu1'})
+
+	for i in 0..5 do
+	menu.items.create([{title: "item #{i}",price: 9.99}])
+	end
+
+end
+
+
+for u in 0..1000 do
+	User.create({email: "u#{u}@seed.com" , password: '12345678' , password_confirmation: '12345678'})
+end
+
+
+
