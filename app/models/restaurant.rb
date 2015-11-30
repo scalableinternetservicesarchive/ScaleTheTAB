65622a2578ab90d761fe888b7b6fe614a704999b
Tushar Bhat <tushar14@gmail.com>
2015-11-29 19:19:56 -0800

    Working Search bar

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,4 +3,8 @@ class Restaurant < ActiveRecord::Base
 	has_many :menus
 	has_attached_file :image, styles: { large: "600x600>", medium: "300x300>", thumb: "150x150#" }
 	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+
+	def self.search(name_field, city_field, zip_code_field)
+		where("name LIKE ? AND city LIKE ? AND zip_code LIKE ?", "%#{name_field}%", "%#{city_field}%", "%#{zip_code_field}%")
+	end
 end
