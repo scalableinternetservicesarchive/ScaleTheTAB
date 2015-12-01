80ed2e0f94797982a5d74ae5d936b269b4886638
Nitish Mehta <Nitish@s-164-67-212-246.resnet.ucla.edu>
2015-11-30 18:59:45 -0800

    Seeds file complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,12 +12,19 @@ include Faker
 total_no_of_users = 5
 total_no_of_owners = 5
 total_no_of_rest = 5
+total_no_of_menus = 5
+total_no_of_items = 5
+total_no_of_tables = 10
 
+
+Menu.delete_all
+Item.delete_all
 Restaurant.delete_all
 LineItem.delete_all
 Cart.delete_all
 Checkout.delete_all
 Tab.delete_all
+Table.delete_all
 User.delete_all
 Owner.delete_all
 
@@ -51,9 +58,11 @@ total_no_of_owners.times{
 #***************************
 # Creating Restaurants
 #***************************
+rest_id_count = 1
 total_no_of_rest.times{
   restaurant = Restaurant.create(
-    name: Company.name,
+    id: rest_id_count,
+    name: Lorem.word,
     image_url: "NULL",
     description: Lorem.sentence(3),
     address: Address.city,
@@ -63,4 +72,48 @@ total_no_of_rest.times{
     owner_id: rand(1..total_no_of_owners),
     image: File.open(Dir.glob(File.join(Rails.root, 'load-tests', '*')).sample)
   )
+  rest_id_count = rest_id_count + 1
+}
+
+#***************************
+# Creating Menus
+#***************************
+menu_id_count = 1
+total_no_of_menus.times{
+  menu = Menu.create(
+    id: menu_id_count,
+    restaurant_id: rand(1..total_no_of_rest),
+    title: Lorem.word,
+    description: Lorem.sentence(3)
+  )
+  menu_id_count = menu_id_count + 1
+}
+
+#***************************
+# Creating Items
+#***************************
+item_id_count = 1
+total_no_of_items.times{
+  item = Item.create(
+    id: item_id_count,
+    title: Lorem.word,
+    description: Lorem.sentence(3),
+    image: File.open(Dir.glob(File.join(Rails.root, 'load-tests', '*')).sample),
+    price: Commerce.price,
+    menu_id: rand(1..total_no_of_menus)
+  )
+  item_id_count = item_id_count + 1
+}
+
+#***************************
+# Creating Tables
+#***************************
+table_id_count = 1
+total_no_of_tables.times{
+  table = Table.create(
+    id: table_id_count,
+    name: Lorem.word,
+    restaurant_id: rand(1..total_no_of_rest)
+  )
+  table_id_count = table_id_count + 1
 }
