53b23b929aba76a2688c9e52bf119ccbc93f76a2
Nitish Mehta <Nitish@wifi-131-179-7-43.host.ucla.edu>
2015-12-01 18:33:09 -0800

    Revert "table to rest foreign key add"
    This reverts commit b2aa9ca88e6761dea8334680b0ab4f22e8e199ea.

--- db/migrate/20151202021527_add_foreign_key_constraint_to_tables.rb
+++
@@ -1,6 +0,0 @@
-class AddForeignKeyConstraintToTables < ActiveRecord::Migration
-  def change
-  	remove_column :tables, :restaurant_id
-  	add_reference :tables, :restaurant, index: true, foreign_key: true
-  end
-end
