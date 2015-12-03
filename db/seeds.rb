21293b84a3913f5d4daaa5da0e4147a26fc1afe2
gorishagh <goris.haghverdian@yahoo.com>
2015-12-02 18:44:23 -0800

    update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,21 +14,21 @@ total_no_of_users = 1000
 total_no_of_owners = 1000
 total_no_of_rest = 1000
 total_no_of_menus = 2000
-total_no_of_items = 30000  
-total_no_of_tables = 3
+total_no_of_items = 10000  
+total_no_of_tables = 2
 
 
 
-LineItem.delete_all
-Item.delete_all
-Menu.delete_all
-Cart.delete_all
-Checkout.delete_all
-Tab.delete_all
-Table.delete_all
-Restaurant.delete_all
-User.delete_all
-Owner.delete_all
+#LineItem.delete_all
+#Item.delete_all
+#Menu.delete_all
+#Cart.delete_all
+#Checkout.delete_all
+#Tab.delete_all
+#Table.delete_all
+#Restaurant.delete_all
+#User.delete_all
+#Owner.delete_all
 
 #***************************
 # Creating Users
@@ -142,7 +142,7 @@ total_no_of_rest.times{
 #***************************
 # Creating Menus
 #***************************
-menu_id_count = total_no_of_rest + 100
+menu_id_count = total_no_of_rest + 2
 total_no_of_menus.times{
   menu = Menu.create(
     id: menu_id_count,
@@ -156,7 +156,7 @@ menu_id_count = menu_id_count - 1
 # #***************************
 # # Creating Items
 # #***************************
-item_id_count = total_no_of_rest + 100
+item_id_count = total_no_of_rest + 2
 total_no_of_items.times{
   item = Item.create(
     id: item_id_count,
