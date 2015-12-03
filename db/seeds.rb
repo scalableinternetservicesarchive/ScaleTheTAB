0efe312b7d6bae3e100972122f41b9a6b70b93e0
Nitish Mehta <Nitish@wifi-131-179-6-90.host.ucla.edu>
2015-12-03 07:53:53 -0800

    merged with servercaching_LT

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
 
-total_no_of_users = 1000
-total_no_of_owners = 1000
-total_no_of_rest = 1000
-total_no_of_menus = 2000
-total_no_of_items = 1000
+total_no_of_users = 100
+total_no_of_owners = 100
+total_no_of_rest = 100
+total_no_of_menus = 200
+total_no_of_items = 100
 total_no_of_tables = 2
 
 
