1096d97548d7bd983c47b4c2cbe28c0edda700a5
gorishagh <goris.haghverdian@yahoo.com>
2015-12-03 10:57:16 -0800

    seed update for multi-app testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,8 +4,8 @@
 total_no_of_users = 1000
 total_no_of_owners = 1000
 total_no_of_rest = 1000
-total_no_of_menus = 2000
-total_no_of_items = 1000
+total_no_of_menus = 20000
+total_no_of_items = 10000
 total_no_of_tables = 2
 
 
