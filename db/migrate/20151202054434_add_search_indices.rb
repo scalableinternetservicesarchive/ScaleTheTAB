db11c532fe71798f30a5d2b5020a4dffd83f4026
Tushar Bhat <tushar14@gmail.com>
2015-12-03 12:41:34 -0800

    AWS fix

--- db/migrate/20151202054434_add_search_indices.rb
+++ db/migrate/20151202054434_add_search_indices.rb
@@ -1,5 +1,8 @@
 class AddSearchIndices < ActiveRecord::Migration
   def change
+  	execute %Q{
+      ALTER TABLE restaurants ENGINE = MYISAM;
+    }
   	add_index :restaurants, :name, type: :fulltext
   	add_index :restaurants, :address, type: :fulltext
   	add_index :restaurants, :zip_code, using: 'btree'
